84f1cd85eb51a425ed001e084cd1c7b676cd2c8d
XXLZopes <afordm99@gmail.com>
2021-04-05 14:31:26 -0700

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# team-generator
+
+# Team Generator
+
+## Description [![license](https://img.shields.io/badge/license-MIT-red.svg)](https://en.wikipedia.org/wiki/MIT_License)
+This is an inquirer based application used to generate an html page that displays and manage employees. At this point in time their is no script to generate the html document.  
+
+## Installation
+To install and run this application first run npm install from the root directory.  To start the application up run npm start from the root.
+## Usage
+
+## Project Testing
+    In the future this application will be testable with jest.
+
+
+## Contact
+[Github](https://github.com/XXLZopess)
+
+Contact me by email at afordm99@gmail.com
--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const Engineer = require('./lib/Engineer');
 const Employee = require('./lib/Employee');
 const teamArray = [];
 
-//Begin choose Role
+//Choose Role
 const chooseRole = () => {
     return inquirer.prompt([
         {
@@ -16,15 +16,20 @@ const chooseRole = () => {
 
         }
     ]).then(choices => {
+        
         let choice = choices.chooseRole
+        console.log(choice)
         if (choice === 'exit') {
-            console.log('Thank you for using Team Generator')
+            console.log(`Thank you for using Team Generator`)
+            console.log('Exiting...')
             return;
         }
-        if (choice === 'Get All') {
+        else if (choice === 'Get All') {
             getAll();
         }
-        chooseAction(choice);
+        else {
+            chooseAction(choice)
+        };
     })
 } 
 chooseRole()
@@ -39,8 +44,9 @@ const chooseAction = (role) => {
         }
     ]).then(choices => {
         let choice = choices.chooseAction;
+        console.log(choice)
        
-        switch (choice.split(' ')[0]) {
+        switch (choice.split(' ')[0].toLowerCase()) {
             case 'add':
                 if (role === 'Manager') {
                     addManager();
@@ -92,6 +98,7 @@ function addManager() {
         const manager = new Manager (response.managerName, response.managerId, response.managerEmail, response.managerOffice)
         console.log(manager.name)
         teamArray.push(manager)
+        console.log(`A new manager has been added, there have now been a total of ${teamArray.length} members added this session.`)
         chooseRole();
     })
 };
@@ -143,7 +150,7 @@ function addEngineer() {
             message: 'What is the engineer\'s GitHub?'
         }
     ]).then(response => {
-        const engineer = new Engineer (response.employeeName, response.employeeId, response.employeeEmail, response.engineerGitHub)
+        const engineer = new Engineer (response.engineerName, response.engineerId, response.engineerEmail, response.engineerGitHub)
         teamArray.push(engineer)
         chooseRole();
     })
@@ -157,19 +164,25 @@ const nameList = results.map(names = () => {
     i++
     return results[i].name
 })
+console.log()
 console.log(`${choice}s`, nameList)
 chooseRole();
 }
 //Get all info
 function getAllByRole(choice) {
     const results = teamArray.filter(team => team.getRole() === choice);
-    if (results.length === 0) {
+    if (results.length <= 0) {
         console.log(`There have not been any ${choice}s logged this session.`)
     } else {console.log(results)}
     chooseRole();
 }
 //Get all employee info
 function getAll() {
+    if (teamArray.length > 0) {
     console.log('All employees from this session:',teamArray)
     chooseRole();
+    } else {
+        console.log('Their have been no employee changes from this session.')
+        chooseRole();
+    }
 }
